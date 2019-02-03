d9a8ed4c5ac37003f2cb9a453b813baaee49f571
Daniel Navarro <danielnavram@gmail.com>
2019-02-03 00:19:50 -0500

    Finish card component and charts, add table component

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ var input = document.querySelector(".searchform__input");
 var btn = document.querySelector(".searchform__btn");
 var cardWidth = document.querySelector(".card").offsetWidth;
 var chart = document.getElementById("myChart");
+var chart2 = document.getElementById("myChart2");
 
 
 /* Events */
@@ -13,22 +14,65 @@ btn.addEventListener('click', function(e) {
 });
 
 /* Stablishing variables and data */
-chart.width = cardWidth;
+//chart.width = cardWidth;
 
 /* Charts JS */
 var chart2d = chart.getContext("2d");
+var chart2d2 = chart2.getContext("2d");
+
+var options = {
+    reponsive: true,
+    maintainAspectRatio: false,
+    legend: {
+        display: false
+    },
+    layout: {
+        padding: {
+            left: 40,
+        }
+    },
+    scales: {
+        xAxes: [{
+            display: false,
+            gridLines: {
+                display: false
+            }
+        }],
+        yAxes: [{
+            display: false,
+            gridLines: {
+                display: false
+            }
+        }]
+    },
+    /* elements: {
+        point: {
+            radius: 0
+        }
+    } */
+};
+
+var data = {
+    labels: ["Mon", "Tue", "Wed", "Thue", "Fri", "Sat", "Sun"],
+    datasets: [
+        {
+            label: "Sales",
+            data: [{x: 0, y: 20}, {x: 59, y: 80}, {x: 80, y: 30}, {x: 20, y: 81}, {x: 56, y: 15}, {x: 55, y: 90}, {x: 40, y: 20}],
+            borderColor: "#FF0068",
+            fill: false,
+            borderWidth: 1,
+        }
+    ],
+}
+
 var firstChart = new Chart(chart2d, {
     type: 'line',
-    data: {
-        labels: ["Mon", "Tue", "Wed", "Thue", "Fri", "Sat", "Sun"],
-        datasets: [
-            {
-                label: "My first data chart",
-                data: [{x: 0, y: 20}, {x: 59, y: 80}, {x: 80, y: 30}, {x: 20, y: 81}, {x: 56, y: 15}, {x: 55, y: 90}, {x: 40, y: 20}],
-                borderColor: "#FF0068",
-                fill: false,
-                borderWidth: 1,
-            }
-        ]
-    }
+    data: data,
+    options: options
+});
+
+var firstChart2 = new Chart(chart2d2, {
+    type: 'line',
+    data: data,
+    options: options
 });
